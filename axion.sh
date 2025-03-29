b5f6c28a856c25dd24eec98a69144b7d43b6b179
developersahilkhanpara <developer.sahilkhanpara@gmail.com>
2025-03-29 15:39:25 +0530

    Update axion.sh

--- axion.sh
+++ axion.sh
@@ -100,6 +100,6 @@ echo "====== Envsetup Done ======="
 echo "===================================="
 echo "        Build Infinity.."
 echo "===================================="
-gk -s
 axion xaga user core
+gk -s
 ax -br -j$(nproc --all)
